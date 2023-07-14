76363e127b03eaeb94ed5a282af3af254905e574
tsaiggo <ttsaiggo@gmail.com>
2023-07-15 01:00:53 +0800

    :tada: Alonica!!!

---
+++ src/main/java/life/tsaiggo/alonica/AlonicaApplication.java
@@ -0,0 +1,13 @@
+package life.tsaiggo.alonica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlonicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlonicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/tsaiggo/alonica/AlonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package life.tsaiggo.alonica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlonicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
